67e84f2720f67c786bab36a51c05ef0ef24c2718
Warzech <warzech2000@gmail.com>
2022-07-16 17:29:11 +0200

    Working functions: Selection, Update, Delete, Add

--- ProjektAplikacji/WPF_MainWindow.xaml.cs
+++ ProjektAplikacji/WPF_MainWindow.xaml.cs
@@ -53,28 +53,27 @@ namespace ProjektAplikacji
             {
                 prodIDtemp=int.Parse(txtItemID.Text);
             }
+            if (txtItemName.Text != String.Empty) {
+                magazyn_produkty itemObject = new magazyn_produkty()
+                {
+                    produkt_id = prodIDtemp,
+                    produkt_nazwa = txtItemName.Text,
+                    firma_id = int.Parse(txtItemFirm.Text),
+                    kategoria_id = int.Parse(txtItemCategory.Text),
+                    model_rok = txtItemYear.Text,
+                    cena = decimal.Parse(txtItemPrice.Text),
 
-            magazyn_produkty itemObject = new magazyn_produkty()
-            {
-                produkt_id = prodIDtemp,
-                produkt_nazwa = txtItemName.Text,
-                firma_id = int.Parse(txtItemFirm.Text),
-                kategoria_id = int.Parse(txtItemCategory.Text),
-                model_rok = txtItemYear.Text,
-                cena = decimal.Parse(txtItemPrice.Text),
-
-            };
-
-            db.magazyn_produkty.Add(itemObject);
-            db.SaveChanges();
-            btnLoadItems_Click(sender, e);
-            
-
+                };
+                db.magazyn_produkty.Add(itemObject);
+                db.SaveChanges();
+                btnLoadItems_Click(sender, e);
+            }
         }
 
         private void btnLoadItems_Click(object sender, RoutedEventArgs e)
         {
             DataBaseEntities db = new DataBaseEntities();
+
             var items = from d in db.magazyn_produkty
                         select new
                         {
@@ -85,67 +84,109 @@ namespace ProjektAplikacji
                             ProductDate = d.model_rok,
                             ProductFirm = d.firma_id,
                         };
+
             this.gridItems.ItemsSource = items.ToList();
 
         }
 
-        private int updateItemID = 1;
-
+        private int updateItemID = 0;
 
         private void gridItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
            
             var tempid = "";
+            int temporaryid = 0;
+
             foreach (var item in gridItems.SelectedItems.AsQueryable())
             {
                 tempid = item.ToString();
                 tempid = tempid.Split('=', ',')[1];
                 tempid = tempid.Trim();
             }
-            
-            // if (this.gridItems.SelectedIndex >= 0)
+
+            DataBaseEntities db = new DataBaseEntities();
+
+            try
             {
-               // if (this.gridItems.SelectedItems.Count >= 0)
+                temporaryid = Convert.ToInt32(tempid);
+            }
+            catch
+            {
+                new Exception(tempid);
+            }
+
+            var r = from d in db.magazyn_produkty
+                    where d.produkt_id == temporaryid
+                    select d;
+
+            if (this.gridItems.SelectedIndex >= 0)
+            {
+                if (this.gridItems.SelectedItems.Count >= 0)
                 {
-                    //if (this.gridItems.SelectedItems.GetType() == typeof(magazyn_produkty))
+
+                    foreach (var mp in r)
                     {
-                        DataBaseEntities db = new DataBaseEntities();
-                        int temporaryid = Convert.ToInt32(tempid);
-
-                        var r = from d in db.magazyn_produkty
-                                where d.produkt_id == temporaryid
-                                select d;
-
-                        foreach (var mp in r)
-                        {
-                            this.txtItemName2.Text = mp.produkt_nazwa;
-                            this.txtItemPrice2.Text = mp.cena.ToString();
-                            this.txtItemID2.Text = mp.produkt_id.ToString();
-                            this.txtItemYear2.Text = mp.model_rok;
-                            this.txtItemCategory2.Text = mp.kategoria_id.ToString();
-                            this.txtItemFirm2.Text = mp.firma_id.ToString();
-                            this.updateItemID = mp.produkt_id;
-                        }
+                        this.txtItemName2.Text = mp.produkt_nazwa;
+                        this.txtItemPrice2.Text = mp.cena.ToString();
+                        this.txtItemID2.Text = mp.produkt_id.ToString();
+                        this.txtItemYear2.Text = mp.model_rok;
+                        this.txtItemCategory2.Text = mp.kategoria_id.ToString();
+                        this.txtItemFirm2.Text = mp.firma_id.ToString();
+                        this.updateItemID = mp.produkt_id;
                     }
+
                 }
             }
-            
+
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
+
             DataBaseEntities db = new DataBaseEntities();
+
             var r = from d in db.magazyn_produkty
                     where d.produkt_id == this.updateItemID
                     select d;
 
-            foreach (var item in r)
-            {
-                MessageBox.Show(item.produkt_nazwa);
-                item.produkt_nazwa = "Update done";
-            }
-            db.SaveChanges();
+            magazyn_produkty obj = r.SingleOrDefault();
 
+            if (obj != null)
+            {
+
+                obj.produkt_nazwa = this.txtItemName2.Text;
+                //obj.produkt_id = Convert.ToInt32(this.txtItemID2.Text);
+                obj.cena = Convert.ToDecimal(this.txtItemPrice2.Text);
+                obj.model_rok = this.txtItemYear2.Text;
+                obj.firma_id = Convert.ToInt32(this.txtItemFirm2.Text);
+                obj.kategoria_id = Convert.ToInt32(this.txtItemCategory2.Text);
+
+            }
+
+            db.SaveChanges();
+            btnLoadItems_Click(sender, e);
+
+        }
+
+        private void btnDeleteRecord_Click(object sender, RoutedEventArgs e)
+        {
+
+            DataBaseEntities db = new DataBaseEntities();
+
+            var r = from d in db.magazyn_produkty
+                    where d.produkt_id == this.updateItemID
+                    select d;
+
+            magazyn_produkty obj = r.SingleOrDefault();
+
+            if(obj != null)
+            {
+
+                db.magazyn_produkty.Remove(obj);
+                db.SaveChanges();
+                btnLoadItems_Click(sender, e);
+
+            }
         }
     }
 }
